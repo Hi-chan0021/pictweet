77646f790901978e83b30b5476e9296d9daf6458
EC2 Default User <ec2-user@ip-172-31-9-26.ap-northeast-1.compute.internal>
2020-08-13 04:41:56 +0000

    ページネーションの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
   
     @nickname = current_user.nickname
-    @tweets = current_user.tweets.order("created_at DESC")
+    @tweets = current_user.tweets.order("created_at DESC").page(params[:page]).per(5)
   end
 end
