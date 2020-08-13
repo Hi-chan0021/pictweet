c8a22eadea5857b93248fa26a92f31f833b20e80
EC2 Default User <ec2-user@ip-172-31-9-26.ap-northeast-1.compute.internal>
2020-08-13 06:13:23 +0000

    ツイートの削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'tweets' => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
   post 'tweets' => 'tweets#create'
+  delete 'tweets/:id' => 'tweets#destroy'
   get   'users/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
