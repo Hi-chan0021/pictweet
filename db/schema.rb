3ea45e0a688bed8a9b5edff7b4c7de650a7aefac
EC2 Default User <ec2-user@ip-172-31-9-26.ap-northeast-1.compute.internal>
2020-08-13 05:05:17 +0000

    ツイート一覧にニックネームの表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_005154) do
+ActiveRecord::Schema.define(version: 2020_08_13_050337) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.integer "user_id"
     t.text "text"
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
